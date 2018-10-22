5c03ebf697cccfa10da4f92245b23d6c94f76027
Javier Isoldi <javierisoldi@gmail.com>
2018-10-22 00:37:08 -0300

    Visitor example removed

--- src/main/java/edu/austral/visitor/ElementVisitor.java
+++
@@ -1,7 +0,0 @@
-package edu.austral.visitor;
-
-public interface ElementVisitor {
-    void visitSquare(Square square);
-    void visitEllipse(Ellipse ellipse);
-    void visitTriangle(Triangle triangle);
-}
--- src/main/java/edu/austral/visitor/Visitable.java
+++
@@ -1,5 +0,0 @@
-package edu.austral.visitor;
-
-public interface Visitable {
-    void accept(ElementVisitor visitor);
-}
