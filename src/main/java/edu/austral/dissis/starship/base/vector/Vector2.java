33898bc154b8e4285a9db72c185f45370774c424
Javier Isoldi <javierisoldi@gmail.com>
2020-10-25 23:10:24 -0300

    Vector fixed

--- src/main/java/edu/austral/dissis/starship/base/vector/Vector2.java
+++ src/main/java/edu/austral/dissis/starship/base/vector/Vector2.java
@@ -11,6 +11,14 @@ public class Vector2 {
         this.y = y;
     }
 
+    public float getAngle() {return (float) (atan2(y, x) - atan2(0, 1));}
+
+    public float getModule() { return (float) Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);}
+
+    public float getX() { return x; }
+
+    public float getY() { return y; }
+
     public Vector2 add(Vector2 other) {return new Vector2(x + other.x, y + other.y);}
 
     public Vector2 subtract(Vector2 other) {return new Vector2(x - other.x, y - other.y);}
@@ -21,14 +29,10 @@ public class Vector2 {
         return new Vector2((float) (x * cos(angle) - y * sin(angle)), (float) (x * sin(angle) + y * cos(angle)));
     }
 
-    public float module() { return (float) Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);}
-
-    public Vector2 unitary() {
-        final float module = module();
+    public Vector2 asUnitary() {
+        final float module = getModule();
         return new Vector2(x / module, y / module);
     }
-
-    float angle() {return (float) (atan2(y, x) - atan2(0, 1));}
 
     public static Vector2 vector(float x, float y) {return new Vector2(x, y);}
 
