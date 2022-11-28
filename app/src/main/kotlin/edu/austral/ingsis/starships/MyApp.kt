57881dda328ac01307dc789d936482809c6729e2
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2022-11-28 20:01:32 -0300

    finished game

--- app/src/main/kotlin/edu/austral/ingsis/starships/MyApp.kt
+++ app/src/main/kotlin/edu/austral/ingsis/starships/MyApp.kt
@@ -28,14 +28,15 @@ import java.util.Collections
 
 private var adapter = Adapter(StateFactory.createEmptyGame(), SPAWN_PROBABILITY)
 private var startingShips= -1;
+private val imageResolver = CachedImageResolver(DefaultImageResolver())
+private val facade = ElementsViewFacade(imageResolver)
+private val keyTracker = KeyTracker()
+
 fun main() {
     launch(MyStarships::class.java)
 }
 
 class MyStarships() : Application() {
-    private val imageResolver = CachedImageResolver(DefaultImageResolver())
-    private val facade = ElementsViewFacade(imageResolver)
-    private val keyTracker = KeyTracker()
 
     override fun start(primaryStage: Stage) {
         val lives = StackPane()
@@ -75,136 +76,10 @@ class MyStarships() : Application() {
     }
 
     private fun addListeners(label: Label, label1: Label, score1: Label, score2: Label) {
-        facade.collisionsListenable.addEventListener(object : EventListener<Collision> {
-            override fun handle(event: Collision) {
-                println("${event.element1Id} ${event.element2Id}")
-                adapter = Adapter(adapter.gameState.collideEntities(event.element1Id,event.element2Id), SPAWN_PROBABILITY)
-            }
-        })
 
-        facade.timeListenable.addEventListener(object : EventListener<TimePassed> {
-
-            override fun handle(event: TimePassed) {
-
-                adapter = adapter.moveEntities()
-
-                updateFacadeEntities()
-
-                updateFacadeShips()
-
-                removeOutOfBoundsEntities()
-
-                updateLives(label, label1)
-
-                updateScore(score1,score2)
-
-                validateVictory(label, label1)
-            }
-
-            private fun updateScore(score1: Label, score2: Label) {
-                score1.text = assertExistence(adapter.gameState.points["STARSHIP-0"].toString())
-                score2.text = assertExistence(adapter.gameState.points["STARSHIP-1"].toString())
-            }
-
-            private fun updateLives(label: Label, label1: Label) {
-                label.text = assertExistence(adapter.gameState.findShipById("STARSHIP-0")?.lives.toString())
-                label1.text = assertExistence(adapter.gameState.findShipById("STARSHIP-1")?.lives.toString())
-            }
-
-            private fun assertExistence(toString: String): String {
-                return if (toString == "null") "" else toString
-            }
-
-            private fun validateVictory(label: Label, label1: Label) {
-                if (adapter.gameState.ships.size == 1 && startingShips>1 && !adapter.gameState.isPaused) {
-                    pause(adapter.gameState.ships[0].id + " won!", "",label, label1)
-                    facade.elements.clear()
-                }
-                if (adapter.gameState.ships.size == 0 && !adapter.gameState.isPaused) {
-                    pause("LOST","", label, label1)
-                    facade.elements.clear()
-                }
-            }
-
-            private fun updateFacadeEntities() {
-                adapter.gameState.entities.forEach {
-                    insertMoverInFacade(it, 0.0, false)
-                }
-            }
-
-            private fun insertMoverInFacade(newMover: Mover<*>, angleOffset: Double, validatePosition: Boolean) {
-                if (facade.elements.containsKey(newMover.id)) {
-                    facade.elements[newMover.id]?.x?.set(validateX(newMover.position.x, validatePosition))
-                    facade.elements[newMover.id]?.y?.set(validateY(newMover.position.y, validatePosition))
-                    facade.elements[newMover.id]?.rotationInDegrees?.set(newMover.rotationInDegrees + angleOffset)
-                } else {
-                    facade.elements[newMover.id] = ModelToUIParser.parseModelToUIModel(newMover)
-                }
-            }
-
-            private fun validateY(y: Double, validatePosition: Boolean): Double {
-                return if (y < 0 && validatePosition) GAME_HEIGHT + y else if (y > GAME_HEIGHT && validatePosition) y - GAME_HEIGHT else y
-            }
-
-            private fun validateX(x: Double, validatePosition: Boolean): Double {
-                return if (x < 0 && validatePosition) GAME_WIDTH + x else if (x > GAME_WIDTH && validatePosition) x - GAME_WIDTH else x
-            }
-
-            private fun removeOutOfBoundsEntities() {
-                adapter.gameState.idsToRemove.forEach {
-                    facade.elements.remove(it)
-                }
-            }
-
-            private fun updateFacadeShips() {
-                adapter.gameState.ships.forEach {
-                    insertMoverInFacade(it.shipMover, 180.0, true)
-                }
-            }
-        })
-
-        keyTracker.keyPressedListenable.addEventListener(object : EventListener<KeyPressed> {
-
-            private var keyBindMap: Map<String, Map<KeyMovement, KeyCode>> = ConfigManager.readBindings()
-            override fun handle(event: KeyPressed) {
-                val key = event.key
-
-                handlePauseResume(key)
-
-                adapter.gameState.ships.forEach { controller ->
-                    keyBindMap[controller.id]?.forEach { (movement, keyCode) ->
-                        if (key == keyCode) {
-                            adapter = Adapter(adapter.gameState.handleShipAction(controller.id, movement), SPAWN_PROBABILITY)
-                        }
-                    }
-                }
-
-
-            }
-
-            private fun handlePauseResume(key: KeyCode) {
-                when (key) {
-                    keyCodeOf(PAUSE_GAME) -> pauseGame()
-                    keyCodeOf(RESUME_GAME) -> resumeGame()
-                    keyCodeOf(SAVE_GAME) -> ConfigManager.saveState(adapter.gameState)
-                    else -> {}
-                }
-            }
-
-            private fun pauseGame() {
-                pause("$PAUSE_GAME: Resume", "$SAVE_GAME: Save", label, label1)
-                adapter = Adapter(adapter.gameState.changeState(), SPAWN_PROBABILITY)
-            }
-
-            private fun resumeGame() {
-                facade.start()
-                adapter = Adapter(adapter.gameState.changeState(), SPAWN_PROBABILITY)
-            }
-
-            private fun keyCodeOf(string: String): Any {
-                return KeyCode.valueOf(string)
-            }
-        })
+        facade.collisionsListenable.addEventListener(MyCollisionListener())
+        facade.timeListenable.addEventListener(MyTimeListener(label, label1, score1, score2))
+        keyTracker.keyPressedListenable.addEventListener(MyKeyPressedListener(label,label1))
     }
 
     private fun startGame(primaryStage: Stage) {
@@ -213,12 +88,7 @@ class MyStarships() : Application() {
         primaryStage.show()
     }
 
-    private fun setGameScene(
-        pane: StackPane,
-        lives: StackPane,
-        layout: VBox,
-        primaryStage: Stage
-    ): Scene {
+    private fun setGameScene(pane: StackPane, lives: StackPane, layout: VBox, primaryStage: Stage): Scene {
         val root = facade.view
         pane.children.addAll(root, lives)
         root.id = "game"
@@ -232,11 +102,7 @@ class MyStarships() : Application() {
         return scene
     }
 
-    private fun initializeMenu(
-        layout: VBox,
-        scene: Scene,
-        pane: StackPane
-    ) {
+    private fun initializeMenu(layout: VBox, scene: Scene, pane: StackPane) {
         addCssToLayout(layout)
 
         val name = Label("Dwightsteroids")
@@ -258,12 +124,7 @@ class MyStarships() : Application() {
         layout.children.addAll(name, options)
     }
 
-    private fun addCssToLabelMenuOption(
-        onePlayer: Label,
-        scene: Scene,
-        pane: StackPane,
-        newGameState: GameState
-    ) {
+    private fun addCssToLabelMenuOption(onePlayer: Label, scene: Scene, pane: StackPane, newGameState: GameState) {
         onePlayer.textFill = Color.WHITE
         onePlayer.style = "-fx-font-family: comic-sans; -fx-font-size: 40"
         onePlayer.setOnMouseEntered {
@@ -317,6 +178,144 @@ class MyStarships() : Application() {
     override fun stop() {
         facade.stop()
         keyTracker.stop()
+    }
+    class MyCollisionListener(): EventListener<Collision>{
+        override fun handle(event: Collision) {
+            println("${event.element1Id} ${event.element2Id}")
+            adapter = Adapter(adapter.gameState.collideEntities(event.element1Id,event.element2Id), SPAWN_PROBABILITY)
+        }
+    }
+
+    class MyKeyPressedListener(var label: Label,var label1: Label): EventListener<KeyPressed>{
+        private var keyBindMap: Map<String, Map<KeyMovement, KeyCode>> = ConfigManager.readBindings()
+        override fun handle(event: KeyPressed) {
+            val key = event.key
+
+            handlePauseResume(key)
+
+            adapter.gameState.ships.forEach { controller ->
+                keyBindMap[controller.id]?.forEach { (movement, keyCode) ->
+                    if (key == keyCode) {
+                        adapter = Adapter(adapter.gameState.handleShipAction(controller.id, movement), SPAWN_PROBABILITY)
+                    }
+                }
+            }
+        }
+
+        private fun handlePauseResume(key: KeyCode) {
+            when (key) {
+                keyCodeOf(PAUSE_GAME) -> pauseGame()
+                keyCodeOf(RESUME_GAME) -> resumeGame()
+                keyCodeOf(SAVE_GAME) -> ConfigManager.saveState(adapter.gameState)
+                else -> {}
+            }
+        }
+
+        private fun pauseGame() {
+            pause("$PAUSE_GAME: Resume", "$SAVE_GAME: Save", label, label1)
+            adapter = Adapter(adapter.gameState.changeState(), SPAWN_PROBABILITY)
+        }
+
+        private fun pause(s: String, s1: String, label: Label, label1: Label) {
+            facade.stop()
+            label.text = s
+            label1.text = s1
+        }
+
+        private fun resumeGame() {
+            facade.start()
+            adapter = Adapter(adapter.gameState.changeState(), SPAWN_PROBABILITY)
+        }
+
+        private fun keyCodeOf(string: String): Any {
+            return KeyCode.valueOf(string)
+        }
+    }
+
+    class MyTimeListener(var label: Label,var label1: Label, var score1: Label, var score2: Label): EventListener<TimePassed>{
+        override fun handle(event: TimePassed) {
+
+            adapter = adapter.moveEntities()
+
+            updateFacadeEntities()
+
+            updateFacadeShips()
+
+            removeOutOfBoundsEntities()
+
+            updateLives(label, label1)
+
+            updateScore(score1,score2)
+
+            validateVictory(label, label1)
+        }
+
+        private fun updateScore(score1: Label, score2: Label) {
+            score1.text = assertExistence(adapter.gameState.points["STARSHIP-0"].toString())
+            score2.text = assertExistence(adapter.gameState.points["STARSHIP-1"].toString())
+        }
+
+        private fun updateLives(label: Label, label1: Label) {
+            label.text = assertExistence(adapter.gameState.findShipById("STARSHIP-0")?.lives.toString())
+            label1.text = assertExistence(adapter.gameState.findShipById("STARSHIP-1")?.lives.toString())
+        }
+
+        private fun assertExistence(toString: String): String {
+            return if (toString == "null") "" else toString
+        }
+
+        private fun validateVictory(label: Label, label1: Label) {
+            if (adapter.gameState.ships.size == 1 && startingShips>1 && !adapter.gameState.isPaused) {
+                pause(adapter.gameState.ships[0].id + " won!", "",label, label1)
+                facade.elements.clear()
+            }
+            if (adapter.gameState.ships.size == 0 && !adapter.gameState.isPaused) {
+                pause("YOU LOST!","", label, label1)
+                facade.elements.clear()
+            }
+        }
+
+        private fun pause(s: String, s1: String, label: Label, label1: Label) {
+            facade.stop()
+            label.text = s
+            label1.text = s1
+        }
+
+        private fun updateFacadeEntities() {
+            adapter.gameState.entities.forEach {
+                insertMoverInFacade(it, 0.0, false)
+            }
+        }
+
+        private fun insertMoverInFacade(newMover: Mover<*>, angleOffset: Double, validatePosition: Boolean) {
+            if (facade.elements.containsKey(newMover.id)) {
+                facade.elements[newMover.id]?.x?.set(validateX(newMover.position.x, validatePosition))
+                facade.elements[newMover.id]?.y?.set(validateY(newMover.position.y, validatePosition))
+                facade.elements[newMover.id]?.rotationInDegrees?.set(newMover.rotationInDegrees + angleOffset)
+            } else {
+                facade.elements[newMover.id] = ModelToUIParser.parseModelToUIModel(newMover)
+            }
+        }
+
+        private fun validateY(y: Double, validatePosition: Boolean): Double {
+            return if (y < 0 && validatePosition) GAME_HEIGHT + y else if (y > GAME_HEIGHT && validatePosition) y - GAME_HEIGHT else y
+        }
+
+        private fun validateX(x: Double, validatePosition: Boolean): Double {
+            return if (x < 0 && validatePosition) GAME_WIDTH + x else if (x > GAME_WIDTH && validatePosition) x - GAME_WIDTH else x
+        }
+
+        private fun removeOutOfBoundsEntities() {
+            adapter.gameState.idsToRemove.forEach {
+                facade.elements.remove(it)
+            }
+        }
+
+        private fun updateFacadeShips() {
+            adapter.gameState.ships.forEach {
+                insertMoverInFacade(it.shipMover, 180.0, true)
+            }
+        }
     }
 
 }
