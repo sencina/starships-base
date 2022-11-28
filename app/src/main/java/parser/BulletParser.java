bcc3a31f33f9c2047a314794a812d3b8be74dcfb
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2022-11-28 16:10:40 -0300

    game complete, missing making methods shorter

--- app/src/main/java/parser/BulletParser.java
+++ app/src/main/java/parser/BulletParser.java
@@ -13,11 +13,11 @@ public class BulletParser implements EntityParser<Bullet> {
     @Override
     public ElementModel toElementModel(Mover<Bullet> entity) {
         return switch (entity.getEntity().getBulletType()){
-            case BULLET -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), entity.getRotationInDegrees(), BULLET_SIZE, BULLET_SIZE, BULLET_COLLIDER_TYPE,BULLET_IMAGE_REF);
-            case LASER -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), entity.getRotationInDegrees(), LASER_SIZE, LASER_SIZE, BULLET_COLLIDER_TYPE,LASER_IMAGE_REF);
-            case ROCKET -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), entity.getRotationInDegrees(), ROCKET_SIZE, ROCKET_SIZE, BULLET_COLLIDER_TYPE,ROCKET_IMAGE_REF);
-            case PRISON_MIKE -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), entity.getRotationInDegrees(), PRISON_MIKE_SIZE, PRISON_MIKE_SIZE, BULLET_COLLIDER_TYPE,PRISON_MIKE_IMAGE_REF);
-            case CUSTOM -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), entity.getRotationInDegrees(), CUSTOM_BULLET_SIZE, CUSTOM_BULLET_SIZE, BULLET_COLLIDER_TYPE,CUSTOM_BULLET_IMAGE_REF);
+            case BULLET -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), BULLET_SIZE, BULLET_SIZE, entity.getRotationInDegrees(), BULLET_COLLIDER_TYPE,BULLET_IMAGE_REF);
+            case LASER -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), LASER_SIZE, LASER_SIZE, entity.getRotationInDegrees(),BULLET_COLLIDER_TYPE,LASER_IMAGE_REF);
+            case ROCKET -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), ROCKET_SIZE, ROCKET_SIZE, entity.getRotationInDegrees(), BULLET_COLLIDER_TYPE,ROCKET_IMAGE_REF);
+            case PRISON_MIKE -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(), PRISON_MIKE_SIZE, PRISON_MIKE_SIZE, entity.getRotationInDegrees(), BULLET_COLLIDER_TYPE,PRISON_MIKE_IMAGE_REF);
+            case CUSTOM -> new ElementModel(entity.getId(), entity.getPosition().getX(), entity.getPosition().getY(),CUSTOM_BULLET_SIZE, CUSTOM_BULLET_SIZE,entity.getRotationInDegrees(), BULLET_COLLIDER_TYPE,CUSTOM_BULLET_IMAGE_REF);
         };
     }
 }
